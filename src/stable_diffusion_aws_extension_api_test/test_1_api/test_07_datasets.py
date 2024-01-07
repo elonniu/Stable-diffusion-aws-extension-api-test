8308f5ec39a9bf5aaf21a7adbf55004d5c60a82f
Jingyi <elonniu@amazon.com>
2024-01-07 12:07:04 +0800

    use pip config

---
+++ src/stable_diffusion_aws_extension_api_test/test_1_api/test_07_datasets.py
@@ -0,0 +1,100 @@
+from __future__ import print_function
+
+import logging
+
+import stable_diffusion_aws_extension_api_test.config as config
+from stable_diffusion_aws_extension_api_test.utils.api import Api
+
+logger = logging.getLogger(__name__)
+
+
+class TestDatasetsApi:
+    def setup_class(self):
+        self.api = Api(config)
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def test_1_list_datasets_without_key(self):
+        resp = self.api.list_datasets()
+
+        assert resp.status_code == 401, resp.dumps()
+        assert resp.json()["message"] == "Unauthorized"
+
+    def test_2_list_datasets_without_auth(self):
+        headers = {"x-api-key": config.api_key}
+        resp = self.api.list_datasets(headers=headers)
+
+        assert resp.status_code == 401, resp.dumps()
+        assert resp.json()["message"] == "Unauthorized"
+
+    def test_3_list_datasets(self):
+        headers = {
+            "x-api-key": config.api_key,
+            "Authorization": config.bearer_token,
+        }
+
+        resp = self.api.list_datasets(headers=headers)
+        assert resp.status_code == 200, resp.dumps()
+        assert 'links' in resp.json()['data'], resp.dumps()
+        assert len(resp.json()['data']["items"]) >= 0
+
+        datasets = resp.json()['data']["items"]
+        for dataset in datasets:
+            resp = self.api.get_dataset(name=dataset['name'], headers=headers)
+            assert resp.status_code == 200, resp.dumps()
+
+    def test_4_get_dataset_missing_name(self):
+        headers = {
+            "x-api-key": config.api_key,
+            "Authorization": config.bearer_token,
+        }
+
+        name = "not_exists"
+
+        resp = self.api.get_dataset(
+            headers=headers,
+            name=name
+        )
+
+        assert resp.status_code == 404, resp.dumps()
+        assert f"dataset {name} is not found" in resp.json()['message']
+
+    def test_5_create_dataset_without_key(self):
+        resp = self.api.create_dataset()
+
+        assert resp.status_code == 403, resp.dumps()
+        assert resp.json()["message"] == "Forbidden"
+
+    def test_6_update_dataset_without_key(self):
+        resp = self.api.update_dataset(dataset_id="dataset_id")
+
+        assert resp.status_code == 403, resp.dumps()
+        assert resp.json()["message"] == "Forbidden"
+
+    def test_7_delete_datasets_without_key(self):
+        headers = {}
+
+        data = {
+            "bad": ['bad'],
+        }
+
+        resp = self.api.delete_datasets(headers=headers, data=data)
+        assert resp.status_code == 403, resp.dumps()
+        assert 'Forbidden' == resp.json()["message"]
+
+    def test_8_delete_datasets_with_bad_request_body(self):
+        headers = {
+            "x-api-key": config.api_key,
+        }
+
+        data = {
+            "bad": ['bad'],
+        }
+
+        resp = self.api.delete_datasets(headers=headers, data=data)
+        assert resp.status_code == 400, resp.dumps()
+
+        assert 'object has missing required properties' in resp.json()["message"]
+        assert 'dataset_name_list' in resp.json()["message"]
--- src/stable_diffusion_aws_extension_api_test/test_3_release/test_1_api/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-sys.path.append("../../Solution-api-test-framework")
-sys.path.append("src/stable_diffusion_aws_extension_api_test/")
