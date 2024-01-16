7430c2d7c56c7f0d26b44432d35c3339ba8da7b0
Jingyi <elonniu@amazon.com>
2024-01-16 13:42:47 +0800

    update tests

--- test_3_clean/test_01_clean_endpoint.py
+++ test_3_clean/test_01_clean_endpoint.py
@@ -1,9 +1,11 @@
 from __future__ import print_function
 
 import logging
+from time import sleep
 
 import config as config
 from utils.api import Api
+from utils.helper import get_endpoint_status, delete_sagemaker_endpoint_new
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -18,17 +20,16 @@ class TestCleanEndpoint:
         pass
 
     def test_1_delete_endpoints(self):
-        headers = {
-            "x-api-key": config.api_key,
-            "Authorization": config.bearer_token
-        }
+        endpoint_name = f"esd-async-{config.endpoint_name}"
+        while True:
+            status = get_endpoint_status(self.api, endpoint_name)
+            if status is None:
+                break
 
-        data = {
-            "endpoint_name_list": [
-                f"esd-async-{config.endpoint_name}"
-            ],
-            "username": config.username
-        }
-
-        resp = self.api.delete_endpoints(headers=headers, data=data)
-        assert resp.status_code == 204
+            if status in ['Creating', 'Updating']:
+                logger.error(f"Endpoint {endpoint_name} is {status}, waiting to delete...")
+                sleep(10)
+            else:
+                delete_sagemaker_endpoint_new(self.api)
+                break
+        pass
