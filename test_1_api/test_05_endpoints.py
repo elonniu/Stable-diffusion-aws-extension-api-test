617a53f1295279013f56d5281ae4cff198a2acea
Jingyi <elonniu@amazon.com>
2024-01-18 21:49:44 +0800

    added real time endpoint delete

--- test_1_api/test_05_endpoints.py
+++ test_1_api/test_05_endpoints.py
@@ -18,8 +18,23 @@ class TestEndpointsApi:
     def teardown_class(cls):
         pass
 
-    def test_0_endpoints_delete_before(self):
+    def test_0_endpoints_async_delete_before(self):
         endpoint_name = f"esd-async-{config.endpoint_name}"
+        while True:
+            status = get_endpoint_status(self.api, endpoint_name)
+            if status is None:
+                break
+
+            if status in ['Creating', 'Updating']:
+                logger.error(f"Endpoint {endpoint_name} is {status}, waiting to delete...")
+                sleep(10)
+            else:
+                delete_sagemaker_endpoint(self.api)
+                break
+        pass
+
+    def test_1_endpoints_real_time_delete_before(self):
+        endpoint_name = f"esd-real-time-{config.endpoint_name}"
         while True:
             status = get_endpoint_status(self.api, endpoint_name)
             if status is None:
