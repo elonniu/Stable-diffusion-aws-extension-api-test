1e8dc726f6bb9fc191be40928c9037d2726b7cb9
Jingyi <elonniu@amazon.com>
2024-02-29 23:31:16 +0800

    update inference instance type

--- config.py
+++ config.py
@@ -50,10 +50,10 @@ logger.info(f"config.dataset_name: {dataset_name}")
 model_name = "test-model"
 logger.info(f"config.model_name: {model_name}")
 
-async_instance_type = os.environ.get("ASYNC_INSTANCE_TYPE", "ml.g4dn.xlarge")
+async_instance_type = os.environ.get("ASYNC_INSTANCE_TYPE", "ml.g5.2xlarge")
 logger.info(f"config.async_instance_type: {async_instance_type}")
 
-real_time_instance_type = os.environ.get("REAL_TIME_INSTANCE_TYPE", "ml.g4dn.xlarge")
+real_time_instance_type = os.environ.get("REAL_TIME_INSTANCE_TYPE", "ml.g5.2xlarge")
 logger.info(f"config.real_time_instance_type: {real_time_instance_type}")
 
 initial_instance_count = "2"
